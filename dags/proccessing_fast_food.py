24a7ebc38a05cd5dcd5adf05e370592e01d04f42
victor <victor.santos.cd@gmail.com>
2023-10-17 19:28:30 -0300

    (dags/proccessing_fast_food.py): Change dag_id

--- dags/proccessing_fast_food.py
+++ dags/proccessing_fast_food.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
-with DAG(dag_id="temp", start_date=datetime(2023, 1, 1), schedule_interval="0 0 * * *", catchup=False) as dag:
+with DAG(dag_id="hello_world_test", start_date=datetime(2023, 1, 1), schedule_interval="0 0 * * *", catchup=False) as dag:
     hello_world = BashOperator(task_id="hello_world", bash_command="echo Hello world!")
 
     @task()
