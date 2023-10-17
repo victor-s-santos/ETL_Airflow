8118cd1fb81eba16d010d02b8c0f34eb4355951a
victor <victor.santos.cd@gmail.com>
2023-10-17 19:24:00 -0300

    (dags/proccessing_fast_food.py): Initialize the first dag creation

---
+++ dags/proccessing_fast_food.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+
+with DAG(dag_id="temp", start_date=datetime(2023, 1, 1), schedule_interval="0 0 * * *", catchup=False) as dag:
+    hello_world = BashOperator(task_id="hello_world", bash_command="echo Hello world!")
+
+    @task()
+    def run_task():
+        print("Hello world, its working!")
+
+    
+    hello_world >> run_task()
