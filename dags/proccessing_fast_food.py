1525a632307cfd6a4ca16dfc99d0255b16d37d41
victor <victor.santos.cd@gmail.com>
2023-10-31 22:38:55 -0300

    (dags/proccessing_fast_food.py): Fix the dag

--- dags/proccessing_fast_food.py
+++ dags/proccessing_fast_food.py
@@ -1,16 +1,18 @@
 from datetime import datetime
-#para conseguir importar
-import sys
-sys.path.insert(0, '/tasks/task_sample.py')
-#fim
 from airflow import DAG
-from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from tasks.task_sample import run_task
-# from tasks.kaggle.export_csv import download_csv
+from tasks.kaggle.export_csv import download_csv
 
 with DAG(dag_id="hello_world_test", start_date=datetime(2023, 1, 1), schedule_interval="0 0 * * *", catchup=False) as dag:
+    dict_credentials = {"username": "username", "key": "key"}
+    full_dict = {
+        "credentials": dict_credentials,
+        "dataset_owner": "joebeachcapital",
+        "dataset_name": "fast-food",
+    }
+
     hello_world = BashOperator(task_id="hello_world", bash_command="echo Hello world!")
     
     python_hello_world = PythonOperator(
@@ -19,15 +21,11 @@ with DAG(dag_id="hello_world_test", start_date=datetime(2023, 1, 1), schedule_in
         dag=dag
     )
     
-    # task_download_csv = PythonOperator(
-    #     task_id="download_csv",
-    #     python_callable=download_csv,
-    #     dag=dag
-    # )
+    task_download_csv = PythonOperator(
+        task_id="download_csv",
+        python_callable=download_csv(**full_dict),
+        dag=dag
+    )
 
     
-    # hello_world >> python_hello_world >> task_download_csv
-
-    hello_world >> python_hello_world
-
-# TODO Arrumar o erro acusado na task_download_csv que é variável de ambiente
+    hello_world >> python_hello_world >> task_download_csv
