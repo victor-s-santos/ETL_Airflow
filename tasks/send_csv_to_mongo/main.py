d02fc43802c598c2cb11d67779593d864ae1d1af
victor <victor.santos.cd@gmail.com>
2023-11-13 19:05:12 -0300

    (tasks/send_csv_to_mongo): Apply logger instead of print

--- tasks/send_csv_to_mongo/main.py
+++ tasks/send_csv_to_mongo/main.py
@@ -1,6 +1,7 @@
 from csv import DictReader
 from tasks.utils.mongo_connection.main import Mongo 
 import os
+import logging
 
 def get_csvs()-> list:
     """Create a list of all csv files in current directory
@@ -12,10 +13,11 @@ def get_csvs()-> list:
     for file in os.listdir():
         if file.endswith(".csv"):
             csv_files_list.append(file)
+    logging.info(f"The list of csv files: {csv_files_list}")
     return csv_files_list
 
 def send_to_mongo():
-    print("Iniciei aqui")
+    logging.info("Initializing send_to_mongo")
     list_of_csv_files = get_csvs()
     mongo_credentials = {
         "user": os.getenv("MONGOUSER"),
@@ -24,17 +26,17 @@ def send_to_mongo():
         "port": os.getenv("MONGOPORT"),
         "db_name": os.getenv("MONGODBNAME"),
     }
-    print(f"Credenciais: {mongo_credentials}")
-    print(f"Lista de csvs: {os.listdir()}")
     mongo_obj = Mongo(credentials=mongo_credentials)
     mongo_obj.connect_to_mongo()
     my_db = mongo_obj.get_connection()
+    logging.info("Connected successfully!")
     
+    n = 0
     for csv_file in list_of_csv_files:
         my_collection = my_db[csv_file]
         with open(csv_file, "r") as file:
             dict_objs = DictReader(file)
             for dicio in dict_objs:
-                print(dicio)
+                n += 1
                 my_collection.insert_one(dict(dicio))
-    print("SUCESSO!") 
+    logging.info(f"The total inserted records is: {n}")
